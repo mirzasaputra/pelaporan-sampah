0c9e5b981237996b9ab31ba3c92ea2d58c8ccf16
mirzasaputra <mirzasaputra066@gmail.com>
2020-12-31 08:51:55 +0700

    Finishing frontend

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,57 +1,5 @@
 $(document).ready(function(){
 
-    $(window).scroll(function(){
-        if($(window).scrollTop() > 60){
-            $('.navbar').addClass('navbar-fixed');
-        } else {
-            $('.navbar').removeClass('navbar-fixed');
-        }
-    })
-
-    $('.home').click(function(e){
-        e.preventDefault()
-        $('.home').addClass('active');
-        $('.about').removeClass('active');
-        $('.prosedur').removeClass('active');
-        $('.contact').removeClass('active');
-        $('html, body').animate({
-            scrollTop: $('#home').offset().top
-        }, 500)
-    })
-
-    $('.about').click(function(e){
-        e.preventDefault()
-        $('.home').removeClass('active');
-        $('.about').addClass('active');
-        $('.prosedur').removeClass('active');
-        $('.contact').removeClass('active');
-        $('html, body').animate({
-            scrollTop: $('#about').offset().top - 150
-        }, 500)
-    })
-
-    $('.prosedur').click(function(e){
-        e.preventDefault()
-        $('.home').removeClass('active');
-        $('.about').removeClass('active');
-        $('.prosedur').addClass('active');
-        $('.contact').removeClass('active');
-        $('html, body').animate({
-            scrollTop: $('#prosedur').offset().top - 150
-        }, 500)
-    })
-
-    $('.contact').click(function(e){
-        e.preventDefault()
-        $('.home').removeClass('active');
-        $('.about').removeClass('active');
-        $('.prosedur').removeClass('active');
-        $('.contact').addClass('active');
-        $('html, body').animate({
-            scrollTop: $('#contact').offset().top - 150
-        }, 500)
-    })
-
     $('#login').submit(function(e){
         e.preventDefault();
         window.location.assign('home.html');
@@ -71,6 +19,10 @@ $(document).ready(function(){
                 window.location.assign('index.html');
             }
         })
+    })
+
+    $(window).on('load', function(){
+        $('.pre-loader').hide();
     })
 
 })
